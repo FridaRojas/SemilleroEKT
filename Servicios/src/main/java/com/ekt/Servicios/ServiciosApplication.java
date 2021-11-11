41fe720e0cdc2ebbf78c30d711e5002b4ab3ef0e
Fernando Rojas <lfernandorojasf@gmail.com>
2021-11-11 17:31:40 -0600

    Configuración y conexión a mongoDB 'Semillero'

--- Servicios/src/main/java/com/ekt/Servicios/ServiciosApplication.java
+++ Servicios/src/main/java/com/ekt/Servicios/ServiciosApplication.java
@@ -1,5 +1,6 @@
 package com.ekt.Servicios;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class ServiciosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiciosApplication.class, args);
 	}
+
 
 }
