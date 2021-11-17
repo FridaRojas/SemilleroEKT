3e40f123832fc49e152cb821dcf97dfbd7347bfe
jorge <jorgearturo001@gmail.com>
2021-11-17 13:08:31 -0600

    Se agrega configuración de Retrofity modelo conversastion.

--- Android/app/src/main/java/com/example/agileus/config/InitialApplication.kt
+++ Android/app/src/main/java/com/example/agileus/config/InitialApplication.kt
@@ -1,7 +1,18 @@
 package com.example.agileus.config
 
 import android.app.Application
+import com.example.agileus.webservices.ConversationApi
 
 class InitialApplication : Application() {
+
+    companion object{
+        lateinit var webServiceGlobal:ConversationApi
+    }
+    override fun onCreate() {
+        super.onCreate()
+
+        //Este objeto ya puede ser accedido en cualquier parte dela app
+        webServiceGlobal = ConfigRetrofit().obtenerConfiguracionRetofit()
+    }
 
 }
---
+++ Android/app/src/main/java/com/example/agileus/webservices/ConversationApi.kt
@@ -0,0 +1,12 @@
+package com.example.agileus.webservices
+
+import com.example.agileus.models.Conversation
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ConversationApi {
+
+    @GET("conversacion/getOne2/id2_id40")
+    fun getConversationOnetoOne(): Call<ArrayList<Conversation>>
+
+}
