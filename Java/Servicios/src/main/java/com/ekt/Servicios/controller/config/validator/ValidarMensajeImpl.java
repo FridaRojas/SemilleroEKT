e72e5066f35fb73c2937396fa9eb68c6b68509a3
Edgar Hernandez <yatoyenmicorreo97@gmail.com>
2021-11-17 11:32:29 -0600

    se creo la clase ValidarMensajeImpl implementando la inteface ValidarMensaje

---
+++ Java/Servicios/src/main/java/com/ekt/Servicios/controller/config/validator/ValidarMensajeImpl.java
@@ -0,0 +1,7 @@
+package com.ekt.Servicios.controller.config.validator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidarMensajeImpl implements ValidarMensaje{
+}
