00d4ea620842bf46e8dfa8a2bce7268abda87589
ecube <9102Lalito|1>
2021-11-17 15:52:47 -0600

    dataclass mensaje buzon

--- Android/app/src/main/java/com/example/agileus/config/ConfigRetrofit.kt
+++ Android/app/src/main/java/com/example/agileus/config/ConfigRetrofit.kt
@@ -1,12 +1,12 @@
 package com.example.agileus.config
 
+import com.example.agileus.utils.Constantes.URL_BASE
 import com.example.agileus.webservices.ConversationApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ConfigRetrofit {
 
-    val URL_BASE = "http://10.97.3.21:6060/servicio/"
 
     fun obtenerConfiguracionRetofit(): ConversationApi {
         var mRetrofit = Retrofit.Builder()
--- Android/app/src/main/java/com/example/agileus/utils/Constantes.kt
+++ Android/app/src/main/java/com/example/agileus/utils/Constantes.kt
@@ -1,4 +1,10 @@
 package com.example.agileus.utils
 
 object Constantes {
+    val URL_BASE = "http://10.97.3.21:6060/servicio/"
+    val URL_BUZON_1=""
+    val URL_BUZON_2=""
+    val URL_Tareas_Enviadas=""
+    val URL_Tareas_Recibidas=""
+
 }
