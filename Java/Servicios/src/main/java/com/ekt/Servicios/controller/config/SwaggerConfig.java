c26a57cc4296ef75258865c79b5bd373e9561a99
Diego A <diegorame3@gmail.com>
2021-11-17 09:29:04 -0600

    Configuracion de mongo

--- Java/Servicios/src/main/java/com/ekt/Servicios/controller/config/SwaggerConfig.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/controller/config/SwaggerConfig.java
@@ -3,6 +3,12 @@ package com.ekt.Servicios.controller.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -19,5 +25,20 @@ public class SwaggerConfig  {
 				.apis(RequestHandlerSelectors.basePackage("com.ekt.Servicios.controller"))
 				.paths(PathSelectors.any()) 
 				.build();
+
+
 	}
+	@Bean
+	public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory, MongoMappingContext context) {
+
+		MappingMongoConverter converter = new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
+		converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
+
+		return mongoTemplate;
+
+	}
+
+
 }
