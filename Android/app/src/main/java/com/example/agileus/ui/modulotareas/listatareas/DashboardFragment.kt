cac989fd29ad2f8f93f0ea0b94946b0016f98b19
jessvazquez <31674161+jessvazquez@users.noreply.github.com>
2021-11-19 09:50:05 -0600

    Paleta de colores

--- Android/app/src/main/java/com/example/agileus/ui/modulotareas/listatareas/DashboardFragment.kt
+++ Android/app/src/main/java/com/example/agileus/ui/modulotareas/listatareas/DashboardFragment.kt
@@ -8,15 +8,15 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.agileus.databinding.FragmentDashboardBinding
+import com.example.agileus.ui.HomeActivityViewModel
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -30,11 +30,25 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+//          //  textView.text = it
+//        })
         return root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        dashboardViewModel.devuelveLista()
+
+        dashboardViewModel.adaptador.observe(viewLifecycleOwner,{
+            binding.recyclerListaTareas.adapter = it
+            binding.recyclerListaTareas.layoutManager = LinearLayoutManager(activity)
+
+        })
+
     }
 
     override fun onDestroyView() {
--- Android/app/src/main/java/com/example/agileus/ui/modulotareas/listatareas/DashboardViewModel.kt
+++ Android/app/src/main/java/com/example/agileus/ui/modulotareas/listatareas/DashboardViewModel.kt
@@ -1,13 +1,51 @@
 package com.example.agileus.ui.modulotareas.listatareas
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.agileus.adapters.ConversationAdapter
+import com.example.agileus.models.Conversation
+import com.example.agileus.ui.modulomensajeria.listacontactos.HomeViewModel
+import com.example.agileus.webservices.dao.ConversationDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DashboardViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    var adaptador = MutableLiveData<ConversationAdapter>()
+    lateinit var lista: ConversationDao
+
+    //Cambiar el tipo de ArrayList a Tarea
+    lateinit var listaConsumida: ArrayList<Conversation>
+
+    init {
+        lista = ConversationDao()
     }
-    val text: LiveData<String> = _text
+
+    fun devuelveLista() {
+        try {
+            viewModelScope.launch {
+                listaConsumida = withContext(Dispatchers.IO) {
+                    lista.recuperarPublicaciones()
+                }
+                if (listaConsumida != null) {
+                    if (listaConsumida.isNotEmpty()) {
+                        adaptador.value =
+                            ConversationAdapter(listaConsumida as ArrayList<Conversation>)
+                    }
+                }
+
+            }
+
+
+        } catch (ex: Exception) {
+            Log.e(HomeViewModel::class.simpleName.toString(), ex.message.toString())
+        }
+
+
+    }
+
 }
