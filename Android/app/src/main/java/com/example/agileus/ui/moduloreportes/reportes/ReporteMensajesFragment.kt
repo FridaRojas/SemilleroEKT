b91d976fd8f6bb0a85b75040273f0f6dee369731
SMGalan-dev <sergalanm@gmail.com>
2021-11-22 14:40:53 -0600

    Diseño de grafica Pie

--- Android/app/src/main/java/com/example/agileus/ui/moduloreportes/reportes/ReporteMensajesFragment.kt
+++ Android/app/src/main/java/com/example/agileus/ui/moduloreportes/reportes/ReporteMensajesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.agileus.ui.moduloreportes.reportes
 
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.transition.TransitionInflater
@@ -13,11 +14,23 @@ import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import com.example.agileus.R
 import com.example.agileus.databinding.ReporteMensajesFragmentBinding
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.components.LegendEntry
+
+
+
 
 class ReporteMensajesFragment : Fragment() {
 
     private lateinit var reporteMensajesViewModel: ReporteMensajesViewModel
     private var _binding: ReporteMensajesFragmentBinding? = null
+    private lateinit var pieChart: PieChart
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -58,7 +71,80 @@ class ReporteMensajesFragment : Fragment() {
             findNavController().navigate(action,  extras)
         }
 
-        //setBarChart()
+        pieChart = binding.pieChart
+
+        initPieChart()
+
+        setDataToPieChart()
     }
+
+
+    private fun initPieChart() {
+        pieChart.setUsePercentValues(true)
+        pieChart.description.text = ""
+        //hollow pie chart
+        pieChart.isDrawHoleEnabled = false
+        pieChart.setTouchEnabled(false)
+        pieChart.setDrawEntryLabels(false)
+
+        pieChart.setUsePercentValues(true)
+        pieChart.isRotationEnabled = false
+        pieChart.setDrawEntryLabels(false)
+
+        // Eliminar cuadritos
+        pieChart.getLegend().isEnabled = false
+
+        //Remove center chart text
+        pieChart.setDrawSliceText(false)
+    }
+
+
+    private fun setDataToPieChart() {
+        pieChart.setUsePercentValues(true)
+        val dataEntries = ArrayList<PieEntry>()
+        dataEntries.add(PieEntry(72f, "Android"))
+        dataEntries.add(PieEntry(26f, "Ios"))
+        dataEntries.add(PieEntry(2f, "Other"))
+
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(resources.getColor(R.color.colorPrimary))
+        colors.add(resources.getColor(R.color.colorSecondary))
+        colors.add(Color.parseColor("#f6E2E0"))
+
+        val dataSet = PieDataSet(dataEntries, "")
+        val data = PieData(dataSet)
+
+        // In Percentage
+        data.setValueFormatter(PercentFormatter())
+        //dataSet.sliceSpace = 3f
+        dataSet.colors = colors
+        pieChart.data = data
+        data.setValueTextSize(0f)
+        //pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+        pieChart.animateY(100, Easing.EaseInOutQuad)
+
+        //create hole in center
+        pieChart.holeRadius = 58f
+        pieChart.transparentCircleRadius = 61f
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setHoleColor(Color.TRANSPARENT)
+
+
+        //add text in center
+        pieChart.setDrawCenterText(false);
+
+        pieChart.invalidate()
+
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
 
 }
