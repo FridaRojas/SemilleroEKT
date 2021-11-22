112ed4570c005b65953c6b9b364be11b0c680c5f
Karla <alrak33@hotmail.com>
2021-11-22 12:08:23 -0600

    Se agrego un boton flotante que abre un mensaje

--- IOS/AgileUs/AgileUs/Tareas/TareasScreen.swift
+++ IOS/AgileUs/AgileUs/Tareas/TareasScreen.swift
@@ -8,9 +8,43 @@
 import UIKit
 
 class TareasScreen: UIViewController {
-
+    
+    private let botonflotante: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+        button.backgroundColor = .systemGreen
+        
+        let image = UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium))
+        
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.setTitleColor(.white, for: .normal)
+        button.layer.shadowRadius = 10
+        button.layer.shadowOpacity = 0.3
+        //button.layer.masksToBounds = true
+        button.layer.cornerRadius = 30
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(botonflotante)
+        botonflotante.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+            botonflotante.frame = CGRect(
+                x: view.frame.size.width - 90,
+                y: view.frame.size.height - 150,
+                width: 60, height: 60)
+        }
+
+       @objc private func didTapButton() {
+        let alert = UIAlertController(title: "Error",
+                                      message: "Busca otra manera" ,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Aceptar", style:  .cancel, handler: nil))
+        present(alert, animated: true )
         
         
         
@@ -18,18 +52,9 @@ class TareasScreen: UIViewController {
 
         
     }
+    
     override func viewDidDisappear(_ animated: Bool) {
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
