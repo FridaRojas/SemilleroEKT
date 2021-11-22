260d67bff625df046eae53085f5f4f706c842cf1
jessvazquez <31674161+jessvazquez@users.noreply.github.com>
2021-11-22 11:53:27 -0600

    Se modifico el URL base y el EndPoint para las tareas.
    Se consumio el servicio para obtener todas las tareas en el RecyclerView de manera exitosa.

--- Android/app/src/main/java/com/example/agileus/webservices/apis/TasksApi.kt
+++ Android/app/src/main/java/com/example/agileus/webservices/apis/TasksApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.GET
 interface TasksApi {
 
     //todo Falta editar el EndPoint para obtener las tareas
-    @GET("servicio.json?alt=media&token=e3076cf4-2c04-4609-ab51-8d3cbffdc6d8")
+    @GET("tareas/getTareas")
     fun getTasks(): Call<ArrayList<Tasks>>
 
 }
