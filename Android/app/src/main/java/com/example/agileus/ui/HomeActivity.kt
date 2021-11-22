fa12fc142e252b1f511ad8f674cd59c3a69ea926
ecube <9102Lalito|1>
2021-11-22 14:14:07 -0600

    Pantallas de buzon v1

--- Android/app/src/main/java/com/example/agileus/ui/HomeActivity.kt
+++ Android/app/src/main/java/com/example/agileus/ui/HomeActivity.kt
@@ -1,10 +1,21 @@
 package com.example.agileus.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.navOptions
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.agileus.R
@@ -34,7 +45,11 @@ class HomeActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+
+//        val navController = navHostFragment.navController
+  //      findViewById<BottomNavigationView>(R.id.bottom_nav)
+    //        .setupWithNavController(navController)
+
         //recuperarPublicaciones()
     }
-
 }
