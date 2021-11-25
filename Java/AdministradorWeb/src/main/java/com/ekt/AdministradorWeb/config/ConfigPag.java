c24517cc3b3788ac431c28fe2201c108c1abd2be
Diego A <diegorame3@gmail.com>
2021-11-25 17:29:24 -0600

    Avance en pantalla de Usuarios, ya se muestran datos

--- Java/AdministradorWeb/src/main/java/com/ekt/AdministradorWeb/config/ConfigPag.java
+++ Java/AdministradorWeb/src/main/java/com/ekt/AdministradorWeb/config/ConfigPag.java
@@ -1,8 +1,23 @@
 package com.ekt.AdministradorWeb.config;
 
 
+
+import com.ekt.AdministradorWeb.entity.User;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 
 @Controller
@@ -10,6 +25,7 @@ public class ConfigPag {
 
         @GetMapping("/login")
         public String login() {
+
             //return "paginas/organigrama/InicioOrganigrama";
              return "elementos/menuLateral";
         }
@@ -20,10 +36,43 @@ public class ConfigPag {
         return "paginas/usuarios/InicioUsuarios";
     }
 
+    @GetMapping("/findAllUsuarios")
+    public String findAllUsuarios(@ModelAttribute ArrayList<User> listaUsuarios, ModelMap model) {
+        Gson gson = new Gson();
+        //ArrayList<User> listaUsuarios = new ArrayList();
+            //se realiza la peticion al back
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:3040/api/user/findAll")
+                .method("GET", null)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String res = response.body().string();
+            System.out.println(res);
+            System.out.println("Peticion exitosa");
+            JSONObject jsonObject= new JSONObject(res);
+            //Separamos la parte de data
+            JSONArray name1 = jsonObject.getJSONArray("data");
 
+            //prueba de casteo
 
+            System.out.println(name1.getJSONObject(1));
+            System.out.println(name1.length());
 
+            for (int i=0;i<name1.length();i++){
+                listaUsuarios.add(gson.fromJson(name1.getJSONObject(i).toString(), User.class));
+            }
 
+            //User a = gson.fromJson(name1.getJSONObject(1).toString(), User.class);
+            //System.out.println(a.getCorreo());
 
-
+        }catch (Exception e){
+            System.out.println("Error al realizar la consulta");
+        }
+        model.addAttribute("listaUsuarios",listaUsuarios);
+        return "paginas/usuarios/InicioUsuarios";
+        //return listaUsuarios;
+    }
 }
