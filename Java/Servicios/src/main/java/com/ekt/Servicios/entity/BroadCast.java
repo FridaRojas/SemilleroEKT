48a1fa36855d438e3c7f05cc8e3af6e0984171dd
Khryz <christianvalentevargas110898@gmail.com>
2021-11-25 11:09:29 -0600

    Se creó la entidad BroadCast

---
+++ Java/Servicios/src/main/java/com/ekt/Servicios/entity/BroadCast.java
@@ -0,0 +1,45 @@
+package com.ekt.Servicios.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "BroadCast")
+public class BroadCast {
+	@Id
+	private String id;
+    private String asunto;
+    private String descripcion;
+    private String idEmisor;
+    private String nombreEmisor;
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getAsunto() {
+		return asunto;
+	}
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getIdEmisor() {
+		return idEmisor;
+	}
+	public void setIdEmisor(String idEmisor) {
+		this.idEmisor = idEmisor;
+	}
+	public String getNombreEmisor() {
+		return nombreEmisor;
+	}
+	public void setNombreEmisor(String nombreEmisor) {
+		this.nombreEmisor = nombreEmisor;
+	}
+}
