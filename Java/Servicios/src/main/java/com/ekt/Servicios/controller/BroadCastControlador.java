93c8e620bf814861fa88fb9863aac84a35a0198e
Khryz <christianvalentevargas110898@gmail.com>
2021-11-25 12:07:18 -0600

    Se agrego clase BroadCastControlador

---
+++ Java/Servicios/src/main/java/com/ekt/Servicios/controller/BroadCastControlador.java
@@ -0,0 +1,10 @@
+package com.ekt.Servicios.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/broadCast")
+public class BroadCastControlador {
+
+}
