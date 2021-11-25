0591d1dd12461696eac0b1823cc1edbb605cfbb3
Frida Rojas Alarcon <frida.rojasalarcon@viep.com.mx>
2021-11-25 17:13:31 -0600

    Login consume servicio de validar el usuario, aun sin validaciones

--- Java/AdministradorWeb/src/main/java/com/ekt/AdministradorWeb/config/ConfigPag.java
+++ Java/AdministradorWeb/src/main/java/com/ekt/AdministradorWeb/config/ConfigPag.java
@@ -1,8 +1,20 @@
 package com.ekt.AdministradorWeb.config;
 
 
+import com.ekt.AdministradorWeb.entity.Respuesta;
+import com.ekt.AdministradorWeb.entity.User;
+import com.google.gson.Gson;
+import okhttp3.*;
+import org.bson.json.JsonObject;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
 
 
 @Controller
@@ -11,7 +23,7 @@ public class ConfigPag {
         @GetMapping("/login")
         public String login() {
             //return "paginas/organigrama/InicioOrganigrama";
-             return "elementos/menuLateral";
+             return "paginas/login";
         }
 
     @GetMapping("/inicioUsuarios")
@@ -20,10 +32,42 @@ public class ConfigPag {
         return "paginas/usuarios/InicioUsuarios";
     }
 
-
-
-
+    @PostMapping("/entrar")
+    public String Valida(@ModelAttribute User us) {
+             //codigo de postman
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        System.out.println("correo: "+us.getCorreo()+"  contras: "+us.getPassword());
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\r\n    \"correo\": \""+us.getCorreo()+"\",\r\n    \"password\": "+us.getPassword()+",\r\n    \"token\":\"wesasasa\"\r\n}\r\n\r\n\r\n");
+        Request request = new Request.Builder()
+                .url("http://localhost:3040/api/user/validate")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println("Peticion exitosa");
+            JSONObject jsonObject= new JSONObject(response.body().string());
+            if (jsonObject.get("data")!=""){
+                return "paginas/usuarios/InicioUsuarios";
+            }else{
+                return "paginas/login";
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return "paginas/login";
+    }
 
 
 
 }
+
+
+
+
+
+
+
+
