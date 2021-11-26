96651961cc1fc93d6e8af5ba539a5b408d848c86
jose becerra <jose.becerra.arg@gmail.com>
2021-11-25 18:02:33 -0600

    organigrama funcional y se crearon paginas para su integracion

--- Java/Servicios/src/main/java/com/ekt/Servicios/controller/GroupController.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/controller/GroupController.java
@@ -47,6 +47,7 @@ public class GroupController {
         }
     }
 
+    @CrossOrigin(origins = "http://localhost:63342/")
     @GetMapping("/buscar/{id}")
     public ResponseEntity<?> buscar(@PathVariable String id){
         if (groupService.buscarPorId(id).isPresent()){
