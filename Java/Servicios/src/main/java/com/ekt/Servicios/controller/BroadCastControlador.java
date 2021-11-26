da57d12b4121aae020d3da64412e7fb1fd4fe6da
Khryz <christianvalentevargas110898@gmail.com>
2021-11-26 14:51:51 -0600

    Se implemento el servicio para enviar un mensaje de usuario broadcast

--- Java/Servicios/src/main/java/com/ekt/Servicios/controller/BroadCastControlador.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/controller/BroadCastControlador.java
@@ -1,14 +1,18 @@
 package com.ekt.Servicios.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import com.ekt.Servicios.entity.BroadCast;
+import com.ekt.Servicios.entity.Mensajes;
 import com.ekt.Servicios.entity.Response;
 import com.ekt.Servicios.repository.BroadCastRepositorio;
 import com.ekt.Servicios.service.BroadCastServicio;
 import com.ekt.Servicios.service.BroadCastServicioImpl;
+import com.ekt.Servicios.service.MensajesService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +32,11 @@ public class BroadCastControlador {
 	@Autowired
 	private BroadCastRepositorio broadCastRepositorio;
 
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private MensajesService mensajesService;
 
 	@GetMapping("/listaUsuarios/{miId}")
 	public ResponseEntity<?> listaUsuariosGeneral(@PathVariable (value = "miId")String miId){
@@ -64,4 +73,32 @@ public class BroadCastControlador {
 		return listBrd;
 	}
 	
+	@PostMapping("/enviarMensaje")
+	public ResponseEntity<?> enviarMensaje(@RequestBody Mensajes mensajeEntrante){
+		//Optional<User> existo = userRepository.validarUsuario(mensajeEntrante.getIDEmisor());
+		
+		Mensajes mensaje = new Mensajes();
+		
+		mensaje.setIDConversacion(mensajeEntrante.getIDEmisor()+"_"+mensajeEntrante.getIDReceptor());
+		mensaje.setConversacionVisible(true);
+		mensaje.setIDEmisor(mensajeEntrante.getIDEmisor());
+		mensaje.setIDReceptor(mensajeEntrante.getIDReceptor());
+		mensaje.setTexto(mensajeEntrante.getTexto());
+		mensaje.setVisible(false);
+		mensaje.setRutaDocumento(null);
+		mensaje.setNombreConversacionReceptor("Conversacion BroadCast");
+		
+		mensaje.setFechaCreacion(mensajeEntrante.getFechaCreacion());
+		mensaje.setStatusCreado(true);
+		
+		mensaje.setFechaEnviado(new Date());
+		mensaje.setStatusEnviado(true);
+		
+		mensaje.setFechaLeido(new Date(0));
+		mensaje.setStatusLeido(false);
+		
+		mensajesService.crearMensaje(mensaje);
+		
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(mensaje);
+	}
 }
