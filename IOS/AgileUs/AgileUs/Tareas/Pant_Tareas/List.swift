18de8cea8bc423cd4a559944d47cbf4b9e2e6f7d
Karla <alrak33@hotmail.com>
2021-11-29 09:44:07 -0600

    Se agrego la pantalla de tareas

---
+++ IOS/AgileUs/AgileUs/Tareas/Pant_Tareas/List.swift
@@ -0,0 +1,41 @@
+//
+//  List.swift
+//  boton_tabla
+//
+//  Created by user203844 on 23/11/21.
+//
+
+import UIKit
+
+class List: UITableViewCell{
+   
+    @IBOutlet weak var Nametask: UILabel!
+    @IBOutlet weak var Persona: UILabel!
+    @IBOutlet weak var Fecha: UILabel!
+    @IBOutlet weak var Prioridad: UILabel!
+    
+   
+
+    //variables de funcionamiento
+   static let identificador = "identificador_List"
+   static func nib() -> UINib { return UINib(nibName: "List", bundle: nil)}
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()}
+    
+        //  Metodos
+    override func setSelected(_ selected: Bool, animated: Bool) {super.setSelected(selected, animated: animated)}
+    // configur
+    func configurar_celda(i:Any)
+    {
+        var info = i as! Datos
+        Nametask.text = info.titulo
+        Persona.text = info.id_receptor
+        Fecha.text = info.fecha_ini
+        Prioridad.text = info.id_tarea
+        
+    }
+}
+
+
+
