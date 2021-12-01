c3eacd12ce4db19e374157285f85cb3f56eafb59
elliot-21 <51519991+elliot-21@users.noreply.github.com>
2021-12-01 11:33:47 -0600

    preparando merge Carlos_A_H_ios

--- IOS/AgileUs/AgileUs/Chats/ChatsScreen.swift
+++ IOS/AgileUs/AgileUs/Chats/ChatsScreen.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 
-class ChatsScreen: UIViewController,UITableViewDelegate, UITableViewDataSource { //se importan las clases abstraptas
+struct Usuarios: Codable
+{
+    let id: Int
+    let website:String
+    let name:String
+   
+}
 
+class ChatsScreen: UIViewController,UITableViewDelegate, UITableViewDataSource { //se importan las clases abstraptas
+   
     @IBOutlet weak var tabla_chats: UITableView!
-    
+    var usuarios = [Usuarios]()
     //se crea un arreglo para poder simular los datos que nos proporcionara el web service
     var datos = [
         [1,"Pancho"],
@@ -30,6 +38,15 @@ class ChatsScreen: UIViewController,UITableViewDelegate, UITableViewDataSource {
         [15,"aaaaa"]
     ]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabla_chats.delegate = self
+        tabla_chats.dataSource = self
+        tabla_chats.register(lista_chats.nib(), forCellReuseIdentifier: lista_chats.identificador)
+        consumir_Servicio_web()
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return datos.count
     }
@@ -48,12 +65,37 @@ class ChatsScreen: UIViewController,UITableViewDelegate, UITableViewDataSource {
         vc.title = "Nombre del contacto"
         navigationController?.pushViewController(vc, animated: true)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tabla_chats.delegate = self
-        tabla_chats.dataSource = self
-        tabla_chats.register(lista_chats.nib(), forCellReuseIdentifier: lista_chats.identificador)
+    func consumir_Servicio_web()
+    {
+        let servicio = "https://10.97.4.165:3040/api/mensajes/listarConversaciones/618e8821c613329636a769ac"
+        // let servicio = "https://jsonplaceholder.typicode.com/users?id=\(1)" si el api nos pediera parametros solo se le concatena dicho parametro(  ?=\(1)  )
+        let url = URL(string: servicio)
+        
+        URLSession.shared.dataTask(with: url!)
+        {data,response,error in
+            do
+            {
+                self.usuarios = try JSONDecoder().decode([Usuarios].self, from: data!)
+                DispatchQueue.main.async
+                {
+                    var cadena = String()
+                    
+                    for item in self.usuarios
+                    {
+                        print("aqui va algo")
+                    }
+                    print("aqui tambien")
+                }
+                
+            }
+            catch{print("Error")}
+        }.resume()
+        
     }
+    
+    
+    
+    
+   
     
 }
