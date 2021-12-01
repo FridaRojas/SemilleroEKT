2b4f13c04604e7fa74d5ccbe1a7331d1130ad9e9
diegoalexisipn <diegoalexisipn@gmail.com>
2021-12-01 11:07:33 -0600

    creación de servicio findById en TaskServiceImpl

--- Java/Servicios/src/main/java/com/ekt/Servicios/service/TaskService.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/service/TaskService.java
@@ -2,6 +2,9 @@ package com.ekt.Servicios.service;
 
 import com.ekt.Servicios.entity.Task;
 
+import java.util.Optional;
+
 public interface TaskService {
     public Task save(Task tarea);
+    public Optional<Task> findById(String id);
 }
--- Java/Servicios/src/main/java/com/ekt/Servicios/service/TaskServiceImpl.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/service/TaskServiceImpl.java
@@ -3,6 +3,9 @@ package com.ekt.Servicios.service;
 import com.ekt.Servicios.entity.Task;
 import com.ekt.Servicios.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public class TaskServiceImpl implements TaskService{
     @Autowired
@@ -12,4 +15,7 @@ public class TaskServiceImpl implements TaskService{
     public Task save(Task tarea) {
         return tareaRepository.save(tarea);
     }
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Task> findById(String id){ return tareaRepository.findById(id); }
 }
