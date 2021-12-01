33bf09eb2e337a6bb6275534a67363b7ff20b7f0
Ivette Mendoza <menii0820@gmail.com>
2021-11-30 18:07:18 -0600

    Se realizó el consumo del servicio de la api de login..

--- Android/app/src/main/java/com/example/agileus/ui/login/LoginActivity.kt
+++ Android/app/src/main/java/com/example/agileus/ui/login/LoginActivity.kt
@@ -18,67 +18,10 @@ import retrofit2.Response
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
 
-    //lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        /*val btnLogin = findViewById<Button>(R.id.btnLogin)
-        val btnSignIn = findViewById<Button>(R.id.btnSignUp)
-*/
-        /*binding.btnLogin.setOnClickListener {
-            goToLoginUser()
-            btnLogin.setOnClickListener { callServiceGetUsers() }
-
-        }
-
-        binding.btnSignUp.setOnClickListener {
-            goToPassword()
-        }
-    }*/
-
-        /*private fun goToLoginUser() {
-        binding.btnLogin.text = getString(R.string.login_login_button)
-
-    }
-
-    private fun goToPassword() {
-        val email = binding.username.text.toString().trim()
-        val password = binding.password.text.toString().trim()
-
-        if (email.trim().isNotEmpty() ) {
-            userServiceLog.getRecuperaDatos(" ", " ")
-            cambiarDeActivividad()
-        }
-    }
-
-    private fun cambiarDeActivividad() {
-        val intento = Intent(applicationContext, HomeActivity::class.java)
-        startActivity(intento)
-        finish()
-    }
-
-
-    fun callServiceGetUsers(): List<User> {
-        //peti a realizar
-        val userService: UserServiceApi = GetUserDao.getUserDao().create(UserServiceApi::class.java)
-        //llamada al metodo declarada en interfaz, metodo de llamada a retrofit.
-        val result: Call<List<User>> = userService.getRecuperaDatos(" ", " ")
-        //generar peticiones sobre servidor para responder y en este caso la llamada la haremos asyncrona por el metodo enqueue
-
-        var userListResponse: Response<List<User>> = result.execute()
-        var listaUsuarios: List<User> = emptyList()
-        if (userListResponse.isSuccessful) {
-            if (userListResponse.body() != null) {
-                listaUsuarios = userListResponse.body()!!
-            }
-        } else {
-            Log.e("Error consumo", userListResponse.message())
-        }
-        return listaUsuarios
-
-    }*/
 
 
     }
