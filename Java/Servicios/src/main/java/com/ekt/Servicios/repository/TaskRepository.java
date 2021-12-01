cab1fe7f2fb14c06dd8fd2dd59ae480bd2260d66
Edson_Garcia <edsoon93gmail.com>
2021-12-01 12:07:55 -0600

    Creacion de query para obtener tareas por prioridad

--- Java/Servicios/src/main/java/com/ekt/Servicios/repository/TaskRepository.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/repository/TaskRepository.java
@@ -2,9 +2,12 @@ package com.ekt.Servicios.repository;
 
 import com.ekt.Servicios.entity.Task;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends MongoRepository<Task,String> {
 
+    @Query(value = "{'prioridad': ?0}")
+    Iterable<Task> findByPriority(String prioridad);
 }
