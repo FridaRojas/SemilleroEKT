d5758388969cdf39bf52297d5c523b01ca91360e
Edson_Garcia <edsoon93gmail.com>
2021-12-01 11:03:01 -0600

    Creacion de servicio save para guardar tareas

--- Java/Servicios/src/main/java/com/ekt/Servicios/service/TaskService.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/service/TaskService.java
@@ -1,4 +1,7 @@
 package com.ekt.Servicios.service;
 
+import com.ekt.Servicios.entity.Task;
+
 public interface TaskService {
+    public Task save(Task tarea);
 }
--- Java/Servicios/src/main/java/com/ekt/Servicios/service/TaskServiceImpl.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/service/TaskServiceImpl.java
@@ -1,4 +1,15 @@
 package com.ekt.Servicios.service;
 
-public class TaskServiceImpl {
+import com.ekt.Servicios.entity.Task;
+import com.ekt.Servicios.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TaskServiceImpl implements TaskService{
+    @Autowired
+    private TaskRepository tareaRepository;
+
+    @Override
+    public Task save(Task tarea) {
+        return tareaRepository.save(tarea);
+    }
 }
