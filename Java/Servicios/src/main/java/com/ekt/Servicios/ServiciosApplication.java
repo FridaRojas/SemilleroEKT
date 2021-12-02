5001b5c52d1d648090af2d6a873042a81ee6f9bb
Gorshalom <jose.becerra.arg@gmail.com>
2021-12-02 17:00:13 -0600

    configuracion para convertir el jar en war y desplegar

--- Java/Servicios/src/main/java/com/ekt/Servicios/ServiciosApplication.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/ServiciosApplication.java
@@ -3,12 +3,18 @@ package com.ekt.Servicios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class ServiciosApplication {
+public class ServiciosApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(ServiciosApplication.class);
+	}
 
 	@PostConstruct
 	void started() {
