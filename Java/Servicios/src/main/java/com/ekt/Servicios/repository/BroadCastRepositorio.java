523111a384e9910550cfaf7297cc8d2eb3489b3a
Edgar Hernandez <yatoyenmicorreo97@gmail.com>
2021-12-07 10:10:10 -0600

    Se estadarisaron las respuestas de los controladores de broadcast y mensajeria al igual que se cambio el metodo de los token para el broadcast y se movieron a la clase implement de broadcast

--- Java/Servicios/src/main/java/com/ekt/Servicios/repository/BroadCastRepositorio.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/repository/BroadCastRepositorio.java
@@ -2,14 +2,16 @@ package com.ekt.Servicios.repository;
 
 import com.ekt.Servicios.entity.Mensajes;
 import com.ekt.Servicios.entity.User;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.ekt.Servicios.entity.BroadCast;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.io.IOException;
 import java.util.Optional;
 
 public interface BroadCastRepositorio extends MongoRepository<BroadCast, String> {
-
-
 }
