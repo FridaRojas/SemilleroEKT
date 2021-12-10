cba58b1f8d3cdf070592cf65ed9a82f62b2d22d0
diegoalexisipn <diegoalexisipn@gmail.com>
2021-12-09 20:07:03 -0600

    Modificación de servicio obtener todas las tareas por Id y estatus de manera ascendente

--- Java/Servicios/src/main/java/com/ekt/Servicios/repository/TaskRepository.java
+++ Java/Servicios/src/main/java/com/ekt/Servicios/repository/TaskRepository.java
@@ -29,17 +29,13 @@ public interface TaskRepository extends MongoRepository<Task,String> {
     @Query(value = "{'id_grupo': ?0,'id_emisor': ?1 }")
     Iterable<Task> getAllByGroupAndIdEmisor(String id_grupo, String id_emisor);
 
-    @Query(value = "{'id_receptor': ?0, 'estatus': ?1}")//Todas las que le fueron asignadas mediante un estatus
+    @Query(value = "{'id_receptor': ?0, 'estatus': ?1}",sort = "{fecha_ini: 1}")//Todas las que le fueron asignadas mediante un estatus
     Iterable<Task> getAllByIdReceptorAndStatus(String id_receptor, String estatus);
 
     @Query(value = "{'id_grupo': ?0,'id_receptor': ?1 }")
     Iterable<Task> getAllByGroupAndIdReceptor(String id_grupo, String id_receptor);
 
-    @Query(value = "{'id_emisor': ?0, 'estatus': ?1}")
+    @Query(value = "{'id_emisor': ?0, 'estatus': ?1}",sort = "{fecha_ini: 1}")
     Iterable<Task> getAllByIdEmisorAndStatus(String id_emisor, String estatus);
-
-    @Query(value = "{\"$orderby\", { 'fecha_ini' : -1 }}")
-    Iterable<Task> getAllOrderedTask();
-
 
 }
