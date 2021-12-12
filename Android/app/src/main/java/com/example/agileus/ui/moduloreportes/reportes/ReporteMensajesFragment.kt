c8fd023d62db6c07d3f5f10246e4f82d04e8ec52
SMGalan-dev <sergalanm@gmail.com>
2021-12-12 15:31:39 -0600

    Arreglo en cambio de sesion, guardado de pantalla en galeria

--- Android/app/src/main/java/com/example/agileus/ui/moduloreportes/reportes/ReporteMensajesFragment.kt
+++ Android/app/src/main/java/com/example/agileus/ui/moduloreportes/reportes/ReporteMensajesFragment.kt
@@ -1,20 +1,31 @@
 package com.example.agileus.ui.moduloreportes.reportes
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Color
-import android.os.Build
-import android.os.Bundle
+import android.net.Uri
+import android.os.*
+import android.provider.MediaStore
+import android.text.format.DateFormat
 import android.transition.TransitionInflater
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.agileus.BuildConfig
 import com.example.agileus.R
 import com.example.agileus.config.InitialApplication.Companion.preferenciasGlobal
 import com.example.agileus.utils.Constantes.tipo_grafica
@@ -37,6 +48,24 @@ import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.formatter.LargeValueFormatter
+import java.io.File
+import java.io.File.separator
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.net.URL
+import java.util.*
+import kotlin.collections.ArrayList
+import android.provider.DocumentsContract
+
+import android.content.ContentUris
+import android.database.Cursor
+
+import android.os.Environment
+
+import android.os.Build
+import java.net.URI
+import java.net.URISyntaxException
+
 
 @RequiresApi(Build.VERSION_CODES.O)
 class ReporteMensajesFragment : Fragment(), ReportesListener, FiltroReportesDialog.FiltroReportesDialogListener {
@@ -77,13 +106,14 @@ class ReporteMensajesFragment : Fragment(), ReportesListener, FiltroReportesDial
         val root: View = binding.root
 
         reporteMensajesViewModel = ViewModelProvider(this).get(ReporteMensajesViewModel::class.java)
+        solicitarPermisos()
 
         //MySharedPreferences.idUsuarioEstadisticas = MySharedPreferences.idUsuario
 
         return root
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as HomeActivity).fragmentSeleccionado = getString(R.string.taskReportFragment)
@@ -105,6 +135,10 @@ class ReporteMensajesFragment : Fragment(), ReportesListener, FiltroReportesDial
             setStadisticName()
             cambiarGrafica(tipo_grafica)
         })
+        binding.btnDownload.setOnClickListener {
+            tomarScreenShot(view)
+        }
+
         binding.btnFiltroReportes.setOnClickListener {
             val newFragment = FiltroReportesDialog(this)
             newFragment.show(requireActivity().supportFragmentManager, "Filtro de Reportes")
@@ -115,7 +149,6 @@ class ReporteMensajesFragment : Fragment(), ReportesListener, FiltroReportesDial
             val extras = FragmentNavigatorExtras(binding.btnReportesMensajes to "report_slide")
             findNavController().navigate(action, extras)
         }
-
     }
 
     fun setStadisticName(){
@@ -124,6 +157,10 @@ class ReporteMensajesFragment : Fragment(), ReportesListener, FiltroReportesDial
                 binding.txtNombreReportes.setText(it.name)
                 Log.d("idUsuarioEstadisticas", it.id)
             }
+        }
+        if (binding.txtNombreReportes.text.isNullOrEmpty()){
+            idUsuarioEstadisticas = TEAM_ID_REPORTES
+            reporteMensajesViewModel.devuelveListaEmpleados(preferenciasGlobal.recuperarIdSesion())
         }
     }
 
@@ -460,6 +497,177 @@ class ReporteMensajesFragment : Fragment(), ReportesListener, FiltroReportesDial
                 vista = 0
                 tipo_grafica = 0
 
+            }
+        }
+    }
+
+
+    lateinit var pathImagen:String
+
+    fun solicitarPermisos(){
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.WRITE_EXTERNAL_STORAGE, false) -> {
+                    Log.d("No permisos", "Permisos Concedido")
+                }
+                permissions.getOrDefault(Manifest.permission.READ_EXTERNAL_STORAGE, false) -> {
+                    Log.d("No permisos", "Permisos Concedido")
+                } else -> {
+                Toast.makeText(context, "Permisos necesarios para el guardado", Toast.LENGTH_SHORT).show()
+            }
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE))
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun tomarScreenShot(view:View){
+        var fecha =  Date()
+        var formato = DateFormat.format("yyyy-MM-dd_hh:mm:ss",fecha)
+
+
+        view.isDrawingCacheEnabled = true
+        val bitmap = Bitmap.createBitmap(view.getDrawingCache())
+        view.isDrawingCacheEnabled = false
+
+
+        if (Build.VERSION.SDK_INT >= 29) {
+            //SC take and save
+            var screeenReportUri = bitmap.saveImage(activity as HomeActivity)
+            val screeenReport = getPath(activity as HomeActivity, screeenReportUri!!).toString()
+            Log.e("RMF filePath", screeenReport)
+            Toast.makeText(context, "Información guardada en galeria", Toast.LENGTH_SHORT).show()
+        }else{
+            try{
+                val dirPath  = Environment.getExternalStorageDirectory().toString() + "/agileus"
+                val fileDir = File(dirPath)
+                if(!fileDir.exists()){
+                    val mkdir = fileDir.mkdir()
+                }
+                val path = "$dirPath/agileus-$formato.jpeg"
+                pathImagen = path
+
+                view.isDrawingCacheEnabled = true
+                val bitmap = Bitmap.createBitmap(view.getDrawingCache())
+                view.isDrawingCacheEnabled = false
+                val imageFile = File(path)
+                var fileOutputStream = FileOutputStream(imageFile)
+                val calidad = 100
+                bitmap.compress(Bitmap.CompressFormat.JPEG,calidad,fileOutputStream)
+                //val bytes =  ByteArrayOutputStream()
+                //fileOutputStream.write(bytes.toByteArray())
+                fileOutputStream.flush()
+                fileOutputStream.close()
+
+                binding.imgCaptura.setImageBitmap(bitmap)
+
+                val content = binding.imgCaptura as View
+                content.isDrawingCacheEnabled = true
+
+                val bitmapShare = content.drawingCache
+                val root = Environment.getExternalStorageDirectory()
+                val cachePath = File(root.absolutePath + "/AgileUs/report_${SystemClock.uptimeMillis()}")
+                try {
+                    cachePath.createNewFile()
+                    val ostream = FileOutputStream(cachePath)
+                    bitmapShare.compress(Bitmap.CompressFormat.JPEG, 100, ostream)
+                    ostream.close()
+                } catch (e: java.lang.Exception) {
+                    Log.e("Error mapa","Al crear bitmap")
+                    e.printStackTrace()
+                }
+                val imageUri = FileProvider.getUriForFile(Objects.requireNonNull(activity as HomeActivity),
+                    BuildConfig.APPLICATION_ID + ".provider", imageFile)
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    var share =  Intent(Intent.ACTION_SEND)
+                    share.type = "image/*"
+                    share.putExtra(Intent.EXTRA_STREAM, imageUri)
+                    startActivity(Intent.createChooser(share,"Compartir captura"))
+                },2000)
+            }catch (e:Exception){
+                Log.e("Error Captura",e.message.toString())
+                e.printStackTrace()
+            }
+
+        }
+
+    }
+
+
+    @Throws(URISyntaxException::class)
+    fun getPath(context: Context, uri: Uri): String? {
+        var uri = uri
+        val needToCheckUri = Build.VERSION.SDK_INT >= 19
+        var selection: String? = null
+        var selectionArgs: Array<String>? = null
+        if (needToCheckUri && DocumentsContract.isDocumentUri(context.applicationContext, uri)) {
+
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":").toTypedArray()
+                val type = split[0]
+                if ("image" == type) {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                } else if ("video" == type) {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                } else if ("audio" == type) {
+                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+                selection = "_id=?"
+                selectionArgs = arrayOf(split[1])
+        }
+        if ("content".equals(uri.scheme, ignoreCase = true)) {
+            val projection = arrayOf(MediaStore.Images.Media.DATA)
+            var cursor: Cursor? = null
+            try {
+                cursor =
+                    context.contentResolver.query(uri, projection, selection, selectionArgs, null)
+                val column_index: Int = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                if (cursor?.moveToFirst()!!) {
+                    return cursor.getString(column_index)
+                }
+            } catch (e: java.lang.Exception) {
+            }
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            return uri.path
+        }
+        return null
+    }
+
+    fun Bitmap.saveImage(context: Context): Uri? {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+        values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/AgileUs")
+        values.put(MediaStore.Images.Media.IS_PENDING, true)
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, "report_${SystemClock.uptimeMillis()}")
+
+        val uri: Uri? = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        if (uri != null) {
+            saveImageToStream(this, context.contentResolver.openOutputStream(uri))
+            values.put(MediaStore.Images.Media.IS_PENDING, false)
+            context.contentResolver.update(uri, values, null, null)
+            Log.d("URI", values.toString())
+            return uri
+        }
+        return null
+    }
+
+
+    fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
+        if (outputStream != null) {
+            try {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
     }
