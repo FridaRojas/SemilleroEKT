36cd5d6c9ea35dd8ebc56be2eaa8923df385e165
c.r.cano <c.r.cano@outlook.com>
2021-12-12 20:10:38 -0600

    - Se eliminaron comentarios

--- Android/app/src/main/java/com/example/agileus/webservices/NetworkConnection.kt
+++ Android/app/src/main/java/com/example/agileus/webservices/NetworkConnection.kt
@@ -1,12 +1,9 @@
 package com.example.agileus.webservices
 
-import android.annotation.TargetApi
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.net.*
-import android.os.Build
 import androidx.lifecycle.LiveData
 
 class NetworkConnection(private val context: Context): LiveData<Boolean>() {
@@ -19,41 +16,11 @@ class NetworkConnection(private val context: Context): LiveData<Boolean>() {
     override fun onActive() {
         super.onActive()
         updateConnection()
-        //when{
-            //Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> {
+
                 connectivityManager.registerDefaultNetworkCallback(connectivityManagerCallback())
-            //}
-            /*Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP->{
-                lollipopNetworkRequest()
-            }*/
-            /*else ->{
-                context.registerReceiver(
-                    netWorkReceiver,
-                    IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-                )
-            }
-        }*/
+
     }
 
-    /*override fun onInactive() {
-        super.onInactive()
-        connectivityManager.unregisterNetworkCallback(connectivityManagerCallback())
-        //connectivityManager.unregisterNetworkCallback(connectivityManagerCallback())
-    }*/
-
-
-    /*@TargetApi (Build.VERSION_CODES.LOLLIPOP)
-    private fun lollipopNetworkRequest (){
-        val requestBuilder= NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET)
-        connectivityManager.registerNetworkCallback(
-            requestBuilder.build(),
-            connectivityManagerCallback()
-        )
-
-    }*/
 
     private fun connectivityManagerCallback(): ConnectivityManager.NetworkCallback{
 
